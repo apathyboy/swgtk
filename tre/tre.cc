8ce008bb9f651a9df64d3f1dbdbb49a1f5ce2f25
Eric Barr <eb@anhstudios.com>
2011-11-12 09:00:05 -0500

    Added implementation for opening a tre file and reading the header.

--- tre/tre.cc
+++ tre/tre.cc
@@ -1,7 +1,39 @@
 
 #include "tre/tre.h"
 
+using namespace std;
 using namespace tre;
 
-Tre::Tre()
-{}
+Tre::Tre(const string& filename)
+{
+    file_stream_.exceptions(ifstream::failbit | ifstream::badbit);
+    file_stream_.open(filename.c_str(), ios_base::binary);
+
+    ReadHeader_(file_stream_);
+}
+
+Tre::~Tre()
+{
+    file_stream_.close();
+}
+
+std::string Tre::GetVersion() const
+{
+    return version_;
+}
+        
+void Tre::ReadHeader_(ifstream& file_stream)
+{
+    std::string file_type;
+
+    file_stream.width(4);
+    file_stream >> file_type;
+
+    if (file_type.compare("EERT") != 0)
+    {
+        throw runtime_error("Invalid tre file format");
+    }
+    
+    file_stream.width(4);
+    file_stream >> version_;
+}
--- tre/tre.h
+++ tre/tre.h
@@ -2,12 +2,27 @@
 #ifndef TRE_TRE_H_
 #define TRE_TRE_H_
 
+#include <cstdint>
+#include <fstream>
+#include <string>
+
 namespace tre 
 {
     class Tre
     {
     public:
-        Tre();  
+        explicit Tre(const std::string& filename);
+        ~Tre();
+
+        std::string GetVersion() const;
+
+    private:
+        Tre();
+
+        void ReadHeader_(std::ifstream& file_stream);
+
+        std::ifstream file_stream_;
+        std::string version_;
     };     
 }  // namespace tre
 
