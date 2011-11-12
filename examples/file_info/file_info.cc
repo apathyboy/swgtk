093e2836b6251728fac623ea8594dd947ebe1e12
Eric Barr <eb@anhstudios.com>
2011-11-12 08:57:55 -0500

    Loads a tre file and prints the version.

--- examples/file_info/file_info.cc
+++ examples/file_info/file_info.cc
@@ -1,5 +1,29 @@
 
-int main()
+#include <iostream>
+#include <memory>
+#include <string>
+#include <tre/tre.h>
+
+using namespace std;
+using namespace tre;
+
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Usage: " << argv[0] << " <path to tre directory>" << endl;
+        exit(0);
+    }
+
+    string tre_directory(argv[1]);
+
+    {
+        string filename = tre_directory + "/patch_01.tre";
+        Tre tre_file(filename);
+
+        cout << "Reading tre file: " << filename << "\n\n";
+        cout << "   Version: " << tre_file.GetVersion() << endl;
+    }
+
     return 0;        
 }
